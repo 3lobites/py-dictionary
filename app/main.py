964ac10711affa182dc674935e78f7afbff419ae
Viktor Deyneka <3lobites@gmail.com>
2023-09-12 20:40:33 +0300

    work_20230912_2

--- app/main.py
+++ app/main.py
@@ -2,19 +2,29 @@ class Dictionary(object):
     def __init__(self):
         self.dict_list = []
 
-    def __setitem__(self, key, value):
-        self.dict_list.append((key, value))
+    def __setitem__(self, *args):
+        if len(args) != 2:
+            raise KeyError("Dictionary must have a key and a value")
+            # print("Dictionary must have a key and a value")
+            # return
+        for index, _ in enumerate(self.dict_list):
+            if _[0] == args[0]:
+                self.dict_list[index] = (_[0], args[1])
+                return
+        self.dict_list.append((args[0], args[1]))
 
     def __getitem__(self, key) -> tuple:
         for _ in self.dict_list:
             if key == _[0]:
                 return _[1]
+        raise KeyError("key not found")
 
     def __len__(self):
         return len(self.dict_list)
 
 
-# test_dict = Dictionary()
-# test_dict.__setitem__("two", 2222)
-# uuu = test_dict.__getitem__("two")
+# dictionary = Dictionary()
+# dictionary["missing_key"]
+# # dictionary.__setitem__("two")
+# # # uuu = dictionary.__getitem__("two")
 # yyy = 0
